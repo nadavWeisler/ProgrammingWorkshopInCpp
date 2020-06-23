e87943d8a4331e1a002d41dd55588bce232e3be7
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-06-23 12:00:48 +0300

    continue ex2

--- ex2/ex2-sol/RecommenderSystem.cpp
+++ ex2/ex2-sol/RecommenderSystem.cpp
@@ -1,11 +1,47 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include "RecommenderSystem.h"
 
 #define USER_NOT_FOUND "USER NOT FOUND"
 #define UNABLE_TO_OPEN_FILE "Unable to open file %s"
+#define SPACE ' '
 
-
-int RecommenderSystem::loadData(std::string moviesAttributesFilePath, std::string userRanksFilePath)
+std::vector<std::string> splitString(std::string& str)
 {
+	size_t pos = 0;
+	std::string subStr;
+	std::string name;
+	std::vector<std::string> result;
+
+	name = str.substr(0, pos);
+	result.push_back(subStr);
+	str.erase(0, pos + 1);
+
+	while ((pos = str.find(SPACE)) != std::string::npos)
+	{
+		subStr = str.substr(0, pos);
+		result.push_back(subStr);
+		str.erase(0, pos + 1);
+	}
+	return result;
+}
+
+int RecommenderSystem::loadData(const std::string& moviesAttributesFilePath,
+	const std::string& userRanksFilePath)
+{
+	std::ifstream moviesFile(moviesAttributesFilePath);
+	std::string line;
+	while (std::getline(moviesFile, line))
+	{
+		std::vector<std::string> splitLine = splitString(line);
+		std::string name = splitLine.at(0);
+		splitLine.erase(splitLine.begin());
+		this->movieRating[name] = splitLine;
+	}
+
+	std::ifstream rankFile(userRanksFilePath);
+
 	return 0;
 }
 std::string RecommenderSystem::recommendByContent(std::string userName)
--- ex2/ex2-sol/RecommenderSystem.h
+++ ex2/ex2-sol/RecommenderSystem.h
@@ -2,18 +2,54 @@
 #define EX2_SOL__RECOMMENDERSYSTEM_H_
 
 #include <string>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <list>
 class RecommenderSystem
 {
  private:
-	int x;
+	/**
+	 *
+	 */
+	std::multimap<std::string, std::pair<std::string, int>> userRating;
+
+	/**
+	 *
+	 */
+	std::unordered_map<std::string, std::map<std::string, int>> movieRating;
 
  public:
-	int loadData(std::string moviesAttributesFilePath, std::string userRanksFilePath);
+	/**
+	 *
+	 * @param moviesAttributesFilePath
+	 * @param userRanksFilePath
+	 * @return
+	 */
+	int loadData(const std::string& moviesAttributesFilePath, const std::string& userRanksFilePath);
 
+	/**
+	 *
+	 * @param userName
+	 * @return
+	 */
 	std::string recommendByContent(std::string userName);
 
+	/**
+	 *
+	 * @param movieName
+	 * @param userName
+	 * @param k
+	 * @return
+	 */
 	int predictMovieScoreForUser(std::string movieName, std::string userName, int k);
 
+	/**
+	 *
+	 * @param userName
+	 * @param k
+	 * @return
+	 */
 	std::string recommendByCF(std::string userName, int k);
 };
 
